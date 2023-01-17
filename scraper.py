023dcb82745bbde0119a7299369cc7f0dcb17cc0
Henry <83106787+henrylin03@users.noreply.github.com>
2023-01-17 21:58:10 +1100

    Build Webscraper for Metacritic data (#1)
    * initial commit of scraper.py script
    
    * add and test extraction of game names successfully; set up scraper script
    
    * simplify extraction of game names by including <h3> tag into xpath for games
    
    * add game names, platforms, release dates, summaries and metascores as python lists, whose indices match up, as, despite many attempts, unable to find the subelements for the above attributes in a main element 'games'
    
    * update variable names
    
    * find all games by table row, then create list of lists of attributes minus metascore
    
    * update extract function to loop through userscore and metascore urls, and loop through all of the pages to append to the value of a dictionary that holds all list of lists
    
    * create dictionary of dataframes - successfully tested
    
    * add function to scraper to output as csvs; generate csvs and removed old file from kaggle

---
+++ scraper.py
@@ -0,0 +1,83 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+
+
+def setup_chrome_driver():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    options.add_argument("--start-maximized")
+    return webdriver.Chrome(
+        options=options, service=Service(ChromeDriverManager().install())
+    )
+
+
+DRIVER = setup_chrome_driver()
+actions = ActionChains(DRIVER)
+
+# as there is no list of games with attributes, across platforms, need to scrape all data by user-score, then meta-score
+def extract():
+    # dictionary to hold the two different types of scores (key) and their games' attributes (value)
+    attribs_dict = {}
+    for s in ["user", "meta"]:
+        url = f"https://www.metacritic.com/browse/games/score/{s}score/all/all"
+        DRIVER.get(url)
+
+        # find count of last page of content - for metacritic.com, this number does not change as you click through the pages
+        last_page = DRIVER.find_element(
+            By.XPATH, ".//*[@class='page last_page']/*[@class='page_num']"
+        ).text
+
+        attribs_dict[s] = []
+        for p in range(0, int(last_page)):
+            # skip reloading first page
+            if p:
+                DRIVER.get(f"{url}/filtered?page={p}")
+
+            # extract all rows from table element, but ignore the table rows (<tr>) that are class="spacer", which are empty
+            games = DRIVER.find_elements(By.XPATH, ".//tr[not(@class='spacer')]")
+
+            # create list of lists of scraped attributes for each row
+            # set maxsplit=5 as there are 6 attributes (5 "slices") in each list
+            attribs_on_page = [g.text.split("\n", maxsplit=5) for g in games]
+            attribs_dict[s] += attribs_on_page
+
+    DRIVER.close()
+    return attribs_dict
+
+
+def generate_dfs(attribs_dict):
+    dfs = {}
+    for k, v in attribs_dict.items():
+        dfs[k] = pd.DataFrame(
+            v,
+            columns=[
+                f"{k}_score",
+                f"{k}_rank",
+                "name",
+                "platform",
+                "release_date",
+                "summary",
+            ],
+        )
+    return dfs
+
+
+def output_csvs(dfs_dict):
+    for k, v in dfs_dict.items():
+        output_path = os.path.join("./input", f"{k}.csv")
+        v.to_csv(output_path, index=False)
+    return
+
+
+if __name__ == "__main__":
+    dfs = generate_dfs(extract())
+    output_csvs(dfs)
