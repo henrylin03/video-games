403456cc780edec3b5c1a8be258d6e2c14a684bd
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-03-09 08:58:12 +1100

    add comments on how to scrape using condensed list

--- scraper.py
+++ scraper.py
@@ -17,38 +17,53 @@ def setup_chrome_driver():
     )
 
 
-DRIVER = setup_chrome_driver()
+def extract():
+    DRIVER = setup_chrome_driver()
+    ## TEST WITH IOS
+    # scrape all games on all platforms by metascore, expanding and extracting user score in each ticket
+    URL_IOS = "https://www.metacritic.com/browse/games/score/metascore/all/ios/filtered?sort=desc&view=condensed"
+    DRIVER.get(URL_IOS)
+
+    last_page = DRIVER.find_element(
+        By.XPATH, ".//*[@class='page last_page']/*[@class='page_num']"
+    ).text
+
+    for page_no in range(int(last_page)):
+        if page_no:
+            DRIVER.get(f"{URL_IOS}&page={page_no}")
+
+    # scrape all games on all platforms by userscore, but only if the game, platform, and release date information has not already been scraped previously
 
 
 # as there is no list of games with attributes, across platforms, need to scrape all data by user-score, then meta-score
-def extract():
-    # dictionary to hold the two different types of scores (key) and their games' attributes (value)
-    attribs_dict = {}
-    for s in ["user", "meta"]:
-        url = f"https://www.metacritic.com/browse/games/score/{s}score/all/all"
-        DRIVER.get(url)
+# def extract():
+#     # dictionary to hold the two different types of scores (key) and their games' attributes (value)
+#     attribs_dict = {}
+#     for s in ["user", "meta"]:
+#         url = f"https://www.metacritic.com/browse/games/score/{s}score/all/all"
+#         DRIVER.get(url)
 
-        # find count of last page of content - for metacritic.com, this number does not change as you click through the pages
-        last_page = DRIVER.find_element(
-            By.XPATH, ".//*[@class='page last_page']/*[@class='page_num']"
-        ).text
+#         # find count of last page of content - for metacritic.com, this number does not change as you click through the pages
+#         last_page = DRIVER.find_element(
+#             By.XPATH, ".//*[@class='page last_page']/*[@class='page_num']"
+#         ).text
 
-        attribs_dict[s] = []
-        for p in range(0, int(last_page)):
-            # skip reloading first page
-            if p:
-                DRIVER.get(f"{url}/filtered?page={p}")
+#         attribs_dict[s] = []
+#         for p in range(0, int(last_page)):
+#             # skip reloading first page
+#             if p:
+#                 DRIVER.get(f"{url}/filtered?page={p}")
 
-            # extract all rows from table element, but ignore the table rows (<tr>) that are class="spacer", which are empty
-            games = DRIVER.find_elements(By.XPATH, ".//tr[not(@class='spacer')]")
+#             # extract all rows from table element, but ignore the table rows (<tr>) that are class="spacer", which are empty
+#             games = DRIVER.find_elements(By.XPATH, ".//tr[not(@class='spacer')]")
 
-            # create list of lists of scraped attributes for each row
-            # set maxsplit=5 as there are 6 attributes (5 "slices") in each list
-            attribs_on_page = [g.text.split("\n", maxsplit=5) for g in games]
-            attribs_dict[s] += attribs_on_page
+#             # create list of lists of scraped attributes for each row
+#             # set maxsplit=5 as there are 6 attributes (5 "slices") in each list
+#             attribs_on_page = [g.text.split("\n", maxsplit=5) for g in games]
+#             attribs_dict[s] += attribs_on_page
 
-    DRIVER.close()
-    return attribs_dict
+#     DRIVER.close()
+#     return attribs_dict
 
 
 def generate_dfs(attribs_dict):
@@ -76,5 +91,6 @@ def output_csvs(dfs_dict):
 
 
 if __name__ == "__main__":
-    dfs = generate_dfs(extract())
-    output_csvs(dfs)
+    # dfs = generate_dfs(extract())
+    extract()
+    # output_csvs(dfs)
