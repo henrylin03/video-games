5e32d0f86b7a517ebadffa8fb20ab900582e221a
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2023-03-09 15:38:58 +1100

    remove actionchains import as not used in scraper

--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
